be7b3d4ed719f29ca8bc5cf71092921239c866b8
Shunsuke Kamada <mod.rab7219@gmail.com>
2022-04-01 16:56:21 +0900

    zxingバーコードスキャン追加

--- app/src/main/java/com/example/firebasescansamples/ui/vanillascan/VanillaScanFragment.kt
+++ app/src/main/java/com/example/firebasescansamples/ui/vanillascan/VanillaScanFragment.kt
@@ -1,38 +1,82 @@
 package com.example.firebasescansamples.ui.vanillascan
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.camera.camera2.internal.compat.quirk.CameraNoResponseWhenEnablingFlashQuirk
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import com.example.firebasescansamples.CameraPermission
 import com.example.firebasescansamples.databinding.FragmentVanillaScanBinding
 
 class VanillaScanFragment : Fragment() {
 
     private var _binding: FragmentVanillaScanBinding? = null
 
+    private lateinit var viewModel: VanillaScanViewModel
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val launcher = registerForActivityResult(
+        CameraPermission.RequestContract(), ::onPermissionResult
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this)[VanillaScanViewModel::class.java]
+        viewModel = ViewModelProvider(this)[VanillaScanViewModel::class.java]
 
         _binding = FragmentVanillaScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+            resumeBarcodeView()
+        }
+
+        if (CameraPermission.hasPermission(requireContext())) {
+            resumeBarcodeView()
+        } else {
+            launcher.launch(Unit)
         }
         return root
+    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        resumeBarcodeView()
+//    }
+
+    override fun onPause() {
+        super.onPause()
+        pauseBarcodeView()
+    }
+
+    private fun resumeBarcodeView() {
+        if(!lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) return
+        binding.barcodeView.resume()
+        binding.barcodeView.decodeSingle(viewModel.getBarcodeCallback())
+    }
+
+    private fun pauseBarcodeView() {
+        binding.barcodeView.pause()
+    }
+
+    private fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            resumeBarcodeView()
+        } else {
+            activity?.finish()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/firebasescansamples/ui/vanillascan/VanillaScanViewModel.kt
+++ app/src/main/java/com/example/firebasescansamples/ui/vanillascan/VanillaScanViewModel.kt
@@ -3,6 +3,10 @@ package com.example.firebasescansamples.ui.vanillascan
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.ResultPoint
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
 
 class VanillaScanViewModel : ViewModel() {
 
@@ -10,4 +14,14 @@ class VanillaScanViewModel : ViewModel() {
         value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun getBarcodeCallback(): BarcodeCallback {
+        return object : BarcodeCallback {
+            override fun barcodeResult(result: BarcodeResult?) {
+                _text.value = result?.text ?: "null"
+            }
+
+            override fun possibleResultPoints(resultPoints: MutableList<ResultPoint>?) {}
+        }
+    }
 }
